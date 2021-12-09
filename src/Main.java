41d8ca1a1a577d8d6f29e132d3aa287ba281e642
Paweł Leszczyński <pawelleszczynskiprogrammer@gmail.com>
2021-12-09 18:39:05 +0100

    ćwiczenie 4

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,24 @@ public class Main {
 
         /* zad.1 proszę dodać kilka złożonych operacji uzywając kilku operatorów arytmetycznych i logicznych */
 
-
+        int a = 4;
+        int b = 1;
+        int c = 6;
+        System.out.println("a + b = "+ (a+b));
+        System.out.println("a + c = "+ (a+c));
+        System.out.println("b + c = "+ (b+c));
+        System.out.println("(b + c) * 416 = "+ (b+c)*416);
+        if(a < b)
+            System.out.println("a jest mniejsze od b");
+        else
+            System.out.println("a jest większe od b");
+        if(a < c)
+            System.out.println("a jest mniejsze od c");
+        else
+            System.out.println("a jest większe od c");
+        if(b < c)
+            System.out.println("b jest mniejsze od c");
+        else
+            System.out.println("b jest większe od c");
     }
 }
