df4bdfb6c70f5f168f3888429d28969614ecb1ea
Paweł Leszczyński <pawelleszczynskiprogrammer@gmail.com>
2021-12-10 17:56:48 +0100

    ćwiczenie 6

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,15 @@ public class Main {
         /* zad1. stworzyć tablicę typu double 5-cio elementową,
         przypisać do niej wartości i wyświetlić */
 
+        double[] tablica = new double[5];
+        tablica[0] = 6.8;
+        tablica[1] = 1.1;
+        tablica[2] = 7.1;
+        tablica[3] = 77.3;
+        tablica[4] = 12.7;
+
+        for (int i = 0; i < 5; i++)
+            System.out.println("następny numer to: " + tablica[i]);
 
     }
 }
